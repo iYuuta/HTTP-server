4f1e5f68f44605d286e939327eaab9219bd956e9
yuuta <youssefayedder@gmail.com>
2025-08-28 20:19:09 +0100

    transcended

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -76,13 +76,13 @@ MIME::MIME() {
 }
 
 Exec::Exec() {
-	_exec[".sh"]	= "/bin/sh";
-	_exec[".bash"]  = "/bin/bash";
-	_exec[".zsh"]   = "/bin/zsh";
+	_exec[".sh"]	= "/usr/bin/sh";
+	_exec[".bash"]  = "/usr/bin/bash";
+	_exec[".zsh"]   = "/usr/bin/zsh";
 	_exec[".py"]	= "/usr/bin/python3";
 	_exec[".php"]   = "/usr/bin/php";
 	_exec[".pl"]	= "/usr/bin/perl";
-	_exec[".js"]	= "/usr/local/bin/node";
+	_exec[".js"]	= "/usr/bin/node";
 }
 
 ERRORS::ERRORS() {
